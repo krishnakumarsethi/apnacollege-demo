518f941fba380a24458821817b80cf3c416bd4c1
krishnakumarsethi <krishnasethi741@gmail.com>
2024-02-22 23:38:00 +0530

    Added a second type of capitalize program KRIshnakuMAR

--- 22-02-2024KrIsHnAKuMaR.py
+++ 22-02-2024KrIsHnAKuMaR.py
@@ -1,10 +1,6 @@
-''' krishna kumar ---> KrIsHnA KuMaR '''
-user=input("Enter the string").split()
-for i in user:
-    i=i.capitalize()
-    for j in range(len(i)):
-        if j%2==0:
-            j=i[j].upper()
-        else:
-            j=i[j].lower()
-        print(j,end=" ")
+''' krishna kumar ---> KRIshna kuMAR '''
+user=input("Enter the string")
+st=user[:3].upper()
+end=user[-3].upper()
+letter=st+user[3:-3]+end
+print(letter)
